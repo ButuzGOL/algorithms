08f689bcef75c8094e15b3de465bd71e1d37360e
ButuzGOL <butuzgol@gmail.com>
2023-03-30 10:13:53 +0300

    Transport 2 levels

--- transport/src/main.rs
+++ transport/src/main.rs
@@ -1,10 +1,19 @@
 // M|i - place
 // N|j - move
+// production_need -> bottom
+// production_place -> right
+// V - bottom
+// U - right
 
 // use serde::{Deserialize, Serialize};
 // use serde_json::Result;
 
+use std::time::Instant;
+
+use serde_json::to_vec;
+
 const EMPTY: i32 = -777;
+const M: i32 = -999;
 
 fn _print_2d(text: &str, vec: &Vec<Vec<i32>>) {
   println!("{}", text);
@@ -60,6 +69,7 @@ fn get_data_if_opened(
   data: &Vec<Vec<i32>>,
   production_place: &Vec<i32>,
   production_need: &Vec<i32>,
+  part_index: Option<[usize; 2]>,
 ) -> (Vec<Vec<i32>>, Vec<i32>, Vec<i32>) {
   let mut new_data = data.clone();
   let mut new_production_place = production_place.clone();
@@ -71,14 +81,29 @@ fn get_data_if_opened(
   }
 
   if diff_value > 0 {
-    for i in 0..new_data.len() {
-      new_data[i].push(0);
+    if let Some(inx) = part_index {
+      for i in 0..new_data.len() {
+        new_data[i].push(if inx[0] > i { M } else { 0 });
+      }
+    } else {
+      for i in 0..new_data.len() {
+        new_data[i].push(0);
+      }
     }
 
     new_production_need.push(diff_value);
   } else {
-    new_data.push(vec![0; data[0].len()]);
-    new_production_place.push(diff_value * -1);
+    if let Some(inx) = part_index {
+      new_data.insert(inx[0], vec![0; data[0].len()]);
+      let len = new_data[0].len();
+      for i in 0..inx[1] {
+        new_data[inx[0]][len - i - 1] = M;
+      }
+      new_production_place.insert(inx[0], diff_value * -1);
+    } else {
+      new_data.push(vec![0; data[0].len()]);
+      new_production_place.push(diff_value * -1);
+    }
   }
 
   return (new_data, new_production_place, new_production_need);
@@ -106,10 +131,13 @@ fn get_send_list_min_price(
 ) -> Vec<Vec<i32>> {
   let mut min_list = Vec::new();
   let mut zero_list = Vec::new();
+  let mut m_list = Vec::new();
   for i in 0..data.len() {
     for j in 0..data[i].len() {
       let val = data[i][j];
-      if val == 0 {
+      if val == M {
+        m_list.push([val, i as i32, j as i32]);
+      } else if val == 0 {
         zero_list.push([val, i as i32, j as i32]);
       } else {
         min_list.push([val, i as i32, j as i32]);
@@ -118,7 +146,7 @@ fn get_send_list_min_price(
   }
 
   min_list.sort();
-  min_list = [min_list, zero_list].concat();
+  min_list = [min_list, zero_list, m_list].concat();
 
   // println!("Min {:?}", min_list);
 
@@ -266,7 +294,9 @@ fn get_gammas(
 
   for i in 0..send_list.len() {
     for j in 0..send_list[i].len() {
-      if send_list[i][j] == 0 {
+      if data[i][j] == M {
+        gammas[i][j] = M;
+      } else if send_list[i][j] == 0 {
         // Alternative way
         // gammas[i][j] = (u_line[i] + v_line[j]) - data[i][j] as i32;
         gammas[i][j] = data[i][j] - (u_line[i] + v_line[j]);
@@ -284,8 +314,8 @@ fn is_optimal(gammas: &Vec<Vec<i32>>) -> bool {
   for i in 0..gammas.len() {
     for j in 0..gammas[i].len() {
       // Alternative way
-      // if gammas[i][j] != EMPTY && gammas[i][j] > 0 {
-      if gammas[i][j] != EMPTY && gammas[i][j] < 0 {
+      // if gammas[i][j] != EMPTY && gammas[i][j] != M && gammas[i][j] > 0 {
+      if gammas[i][j] != EMPTY && gammas[i][j] != M && gammas[i][j] < 0 {
         result = false;
         break;
       }
@@ -305,8 +335,8 @@ fn get_matrix_indexes(
   for i in 0..gammas.len() {
     for j in 0..gammas[i].len() {
       // Alternative way
-      // if max_delta < gammas[i][j] && gammas[i][j] != EMPTY {
-      if max_delta > gammas[i][j] && gammas[i][j] != EMPTY {
+      // if max_delta < gammas[i][j] && gammas[i][j] != EMPTY && gammas[i][j] != M {
+      if max_delta > gammas[i][j] && gammas[i][j] != EMPTY && gammas[i][j] != M {
         max_delta = gammas[i][j];
         index_max_delta = [i, j];
       }
@@ -760,6 +790,7 @@ fn get_path(
   send_list: &Vec<Vec<i32>>,
   production_place: &Vec<i32>,
   production_need: &Vec<i32>,
+  part_index: Option<[usize; 2]>,
 ) -> Vec<Vec<i32>> {
   let mut result = send_list.clone();
 
@@ -769,30 +800,44 @@ fn get_path(
   }
 
   if diff_val > 0 {
-    for el in &mut result {
-      el.pop();
+    if let Some(inx) = part_index {
+      // for el in &mut result {
+      //   el.pop();
+      // }
+    } else {
+      for el in &mut result {
+        el.pop();
+      }
     }
+
     return result;
   }
 
-  result.pop();
+  if let Some(inx) = part_index {
+    // result.remove(inx[0]);
+  } else {
+    result.pop();
+  }
+
   return result;
 }
 
-fn transport_task<const N0: usize, const M: usize, const N: usize>(
-  data: [[i32; N]; M],
-  production_place: [i32; M],
-  production_need: [i32; N],
-  matrix_indexes_list: [Vec<[usize; 2]>; N0],
+fn transport_task<const N: usize>(
+  data: Vec<Vec<i32>>,
+  production_place: Vec<i32>,
+  production_need: Vec<i32>,
+  matrix_indexes_list: [Vec<[usize; 2]>; N],
   result_price: i32,
-  is_diagonal: bool,
+  part_index: Option<[usize; 2]>,
 ) {
-  let production_place_initial = production_place.clone().to_vec();
-  let production_need_initial = production_need.clone().to_vec();
+  let start = Instant::now();
 
-  let mut data = _convert_nested_a_v(data);
-  let mut production_place = production_place.to_vec();
-  let mut production_need = production_need.to_vec();
+  let production_place_initial = production_place.clone();
+  let production_need_initial = production_need.clone();
+
+  let mut production_place = production_place.clone();
+  let mut production_need = production_need.clone();
+  let mut data = data.clone();
 
   _print_2d("Data", &data);
   println!("Production place {:?}", &production_place);
@@ -802,7 +847,10 @@ fn transport_task<const N0: usize, const M: usize, const N: usize>(
   println!("Closed: {:?}", closed);
 
   (data, production_place, production_need) =
-    get_data_if_opened(&data, &production_place, &production_need);
+    get_data_if_opened(&data, &production_place, &production_need, part_index);
+  _print_2d("Data", &data);
+  println!("Production place {:?}", &production_place);
+  println!("Production need {:?}", &production_need);
 
   fn go_cycle(
     data: &Vec<Vec<i32>>,
@@ -814,6 +862,7 @@ fn transport_task<const N0: usize, const M: usize, const N: usize>(
     initial_gammas: &Vec<Vec<i32>>,
     index: i32,
     skip_matrix_indexes: &Vec<[usize; 2]>,
+    part_index: Option<[usize; 2]>,
   ) -> (Vec<Vec<i32>>, Vec<Vec<i32>>, bool, i32, bool) {
     println!("---Cycle--- {}", index);
     let mut send_list = initial_send_list.clone();
@@ -864,6 +913,7 @@ fn transport_task<const N0: usize, const M: usize, const N: usize>(
         &send_list,
         &production_place_initial,
         &production_need_initial,
+        part_index,
       );
       _print_2d("Path", &path);
     }
@@ -890,6 +940,7 @@ fn transport_task<const N0: usize, const M: usize, const N: usize>(
       &gammas,
       index,
       &Vec::new(),
+      part_index,
     );
 
     index += 1;
@@ -923,6 +974,7 @@ fn transport_task<const N0: usize, const M: usize, const N: usize>(
         &gammas_alternative,
         index,
         &skipped_matrix_indexes,
+        part_index,
       );
       index += 1;
       alternative_index += 1;
@@ -934,13 +986,58 @@ fn transport_task<const N0: usize, const M: usize, const N: usize>(
   }
 
   assert_eq!(price, result_price, "Error result");
+
+  let duration = start.elapsed();
+  println!("---Duration--- {:?}", duration);
+}
+
+fn get_data_based_double(
+  price_to_time: &Vec<Vec<i32>>,
+  price_from_time: &Vec<Vec<i32>>,
+  a_to_b: &Vec<i32>,
+  a_n: Vec<i32>,
+  d_n: Vec<i32>,
+  b_n: Vec<i32>,
+) -> (Vec<Vec<i32>>, Vec<i32>, Vec<i32>) {
+  let mut data = price_to_time.clone();
+  for inx in 0..data.len() {
+    for el in a_to_b.iter() {
+      if inx == 0 {
+        data[inx].push(*el);
+      } else {
+        data[inx].push(M);
+      }
+    }
+  }
+
+  let next_index = data.len();
+  for i in 0..price_from_time.len() {
+    data.push(Vec::new());
+    for j in 0..price_to_time[0].len() {
+      data[next_index + i].push(if j == i { 0 } else { M });
+    }
+  }
+
+  for i in 0..price_from_time.len() {
+    for j in 0..price_from_time[i].len() {
+      data[next_index + i].push(price_from_time[i][j]);
+    }
+  }
+
+  let mut production_place = a_n.clone();
+  production_place.extend(d_n.clone());
+
+  let mut production_need = d_n.clone();
+  production_need.extend(b_n.clone());
+
+  (data, production_place, production_need)
 }
 
 fn main() {
   fn task_00() {
-    let data = [[1, 3, 2, 4], [2, 1, 4, 3], [3, 5, 6, 1]];
-    let production_place = [35, 50, 15];
-    let production_need = [30, 10, 20, 40];
+    let data = vec![vec![1, 3, 2, 4], vec![2, 1, 4, 3], vec![3, 5, 6, 1]];
+    let production_place = vec![35, 50, 15];
+    let production_need = vec![30, 10, 20, 40];
 
     println!("\nTask youtube closed https://www.youtube.com/watch?v=1jBa_2IYDNY");
 
@@ -950,14 +1047,14 @@ fn main() {
       production_need,
       [[[1, 0], [0, 0], [0, 2], [1, 2]].to_vec()],
       185,
-      false,
+      None,
     );
   }
 
   fn task_0() {
-    let data = [[2, 4, 1, 6], [2, 2, 4, 3], [1, 2, 3, 2]];
-    let production_place = [70, 80, 50];
-    let production_need = [40, 60, 25, 75];
+    let data = vec![vec![2, 4, 1, 6], vec![2, 2, 4, 3], vec![1, 2, 3, 2]];
+    let production_place = vec![70, 80, 50];
+    let production_need = vec![40, 60, 25, 75];
 
     println!("\nTask 0");
     // [
@@ -981,15 +1078,15 @@ fn main() {
         [[0, 1], [0, 3], [1, 3], [1, 1]].to_vec(),
       ],
       410,
-      false,
+      None,
     );
   }
 
   // degenerated
   fn task_1() {
-    let data = [[2, 4, 1, 6], [2, 2, 4, 3], [1, 2, 3, 2]];
-    let production_place = [50, 20, 30];
-    let production_need = [30, 10, 25, 35];
+    let data = vec![vec![2, 4, 1, 6], vec![2, 2, 4, 3], vec![1, 2, 3, 2]];
+    let production_place = vec![50, 20, 30];
+    let production_need = vec![30, 10, 25, 35];
 
     println!("\n=====> Task 1");
     // Diagonal
@@ -1019,14 +1116,14 @@ fn main() {
       production_need,
       [[[2, 3], [2, 0], [0, 0], [0, 3]].to_vec()],
       180,
-      false,
+      None,
     );
   }
 
   fn task_2() {
-    let data = [[4, 3, 5], [8, 7, 6], [4, 5, 9], [10, 9, 7]];
-    let production_place = [60, 70, 30, 70];
-    let production_need = [90, 90, 50];
+    let data = vec![vec![4, 3, 5], vec![8, 7, 6], vec![4, 5, 9], vec![10, 9, 7]];
+    let production_place = vec![60, 70, 30, 70];
+    let production_need = vec![90, 90, 50];
 
     println!("\n=====> Task 2");
     // [
@@ -1052,37 +1149,41 @@ fn main() {
         [[0, 0], [0, 1], [1, 1], [1, 2], [3, 2], [3, 0]].to_vec(),
       ],
       1380,
-      false,
+      None,
     );
   }
 
   // opened/not degenerated
   fn task_000() {
-    let data = [[4, 6, 3], [5, 5, 2]];
-    let production_place = [2000, 2500];
-    let production_need = [1000, 1300, 1200];
+    let data = vec![vec![4, 6, 3], vec![5, 5, 2]];
+    let production_place = vec![2000, 2500];
+    let production_need = vec![1000, 1300, 1200];
 
     println!(
       "\n=====> Task youtube opened not degenerated https://www.youtube.com/watch?v=qz3Vu1TR4Jk"
     );
-    transport_task(data, production_place, production_need, [], 12900, false);
+    transport_task(data, production_place, production_need, [], 12900, None);
   }
 
   // opened
   fn task_3() {
-    let data = [[700, 400], [200, 300], [100, 200]];
-    let production_place = [4500, 3200, 5500];
-    let production_need = [4700, 6500];
+    let data = vec![vec![700, 400], vec![200, 300], vec![100, 200]];
+    let production_place = vec![4500, 3200, 5500];
+    let production_need = vec![4700, 6500];
 
     println!("\n=====> Task 3");
-    transport_task(data, production_place, production_need, [], 2590000, false);
+    transport_task(data, production_place, production_need, [], 2590000, None);
   }
 
   // opened
   fn task_4() {
-    let data = [[700, 400, 500], [200, 300, 400], [100, 200, 100]];
-    let production_place = [4000, 3000, 5000];
-    let production_need = [4700, 6500, 3500];
+    let data = vec![
+      vec![700, 400, 500],
+      vec![200, 300, 400],
+      vec![100, 200, 100],
+    ];
+    let production_place = vec![4000, 3000, 5000];
+    let production_need = vec![4700, 6500, 3500];
 
     println!("\n=====> Task 4");
     // [
@@ -1107,15 +1208,135 @@ fn main() {
         [[3, 1], [3, 0], [1, 0], [1, 1]].to_vec(),
       ],
       2700000,
-      false,
+      None,
     );
+  }
+
+  fn task_5() {
+    let data = vec![vec![1, 3, 1, 2], vec![2, 1, 4, 3], vec![5, 3, 2, 3]];
+    let production_place = vec![100, 130, 270];
+    let production_need = vec![80, 120, 240, 100];
+
+    println!("\n=====> Task 4");
+
+    transport_task(data, production_place, production_need, [], 840, None);
+  }
+
+  fn task_6_d() {
+    let price_to_time = vec![vec![3, 6], vec![6, 9]];
+    let price_from_time = vec![vec![4, 7, 10], vec![10, 13, 16]];
+    let a_to_b = vec![1, 2, 3];
+    let a_n = vec![15, 105];
+    let d_n = vec![80, 60];
+    let b_n = vec![45, 45, 45];
+
+    // [
+    //   [3, 6, 1, 2, 3,    15],
+    //   [6, 9, M, M, M,    105],
+    //   [0, M, 4, 7, 10,   80],
+    //   [M, 0, 10, 13, 16, 60]
+    //   [80, 60, 45, 45, 45 ]
+    // ]
+
+    let (data, production_place, production_need) =
+      get_data_based_double(&price_to_time, &price_from_time, &a_to_b, a_n, d_n, b_n);
+
+    println!("\n=====> Task 6 double");
+
+    transport_task(
+      data,
+      production_place,
+      production_need,
+      [],
+      1785,
+      Some([price_to_time.len(), a_to_b.len()]),
+    );
+
+    // ! A1 =>(15) B3
+    // ! A2 =>(80) D1
+    // ! A2 =>(25) D2
+    // ! A3 =>(15) D2
+    // ! D1 =>(35) B1
+    // ! D1 =>(45) B2
+    // ! D2 =>(10) B1
+    // ! D2 =>(30) B3
+
+    // [
+    // [0, 0, 0, 0, 15]
+    // [80, 25, 0, 0, 0]
+    // [0, 15, 0, 0, 0]
+    // [0, 0, 35, 45, 0]
+    // [0, 20, 10, 0, 30]
+    // ]
+  }
+
+  fn task_rgr() {
+    let data = vec![vec![1, 3, 1, 2], vec![2, 1, 4, 3], vec![5, 3, 2, 3]];
+    let production_place = vec![100, 130, 270];
+    let production_need = vec![80, 120, 240, 100];
+
+    println!("\n=====> Task Rgr 0");
+
+    transport_task(data, production_place, production_need, [], 840, None);
+  }
+
+  fn task_rgr_d() {
+    let price_to_time = vec![vec![3, 7], vec![11, 8], vec![5, 2]];
+    let price_from_time = vec![vec![4, 6], vec![9, 10]];
+    let a_to_b = vec![1, M];
+    let a_n = vec![25, 35, 75];
+    let d_n = vec![100, 40];
+    let b_n = vec![20, 20];
+
+    // [
+    //   [3, 7, 1, M], 25
+    //   [11, 8, M, M], 35
+    //   [5, 2, M, M], 75
+    //   [0, M, 4, 6], 100
+    //   [M, 0, 9, 10], 40
+    //   100, 40, 20, 20
+    // ]
+
+    let (data, production_place, production_need) =
+      get_data_based_double(&price_to_time, &price_from_time, &a_to_b, a_n, d_n, b_n);
+
+    println!("\n=====> Task Rgr double");
+
+    transport_task(
+      data,
+      production_place,
+      production_need,
+      [],
+      795,
+      Some([price_to_time.len(), a_to_b.len()]),
+    );
+
+    // A1 =>(5) D1
+    // A1 =>(20) B1
+    // A2 =>(35) D1
+    // A3 =>(35) D1
+    // A3 =>(40) D2
+    // D1 =>(20) B2
+    // D1 =>(55) B3
+    // D2 =>(40) B3
+
+    // [
+    // [5, 0, 20, 0, 0]
+    // [35, 0, 0, 0, 0]
+    // [35, 40, 0, 0, 0]
+    // [25, 0, 0, 20, 55]
+    // [0, 0, 0, 0, 40]
+    // ]
   }
 
   // task_00();
   // task_0();
   // task_1();
   // task_2();
-  task_000();
+  // task_000();
   // task_3();
   // task_4();
+  // task_6_d();
+  // task_rgr();
+  task_rgr_d();
 }
